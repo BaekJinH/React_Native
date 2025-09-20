cc7062d6eea87be1edb06b98e224f5dea139771a
BaekJinH <tmdfyd6868@naver.com>
2025-09-21 04:52:13 +0900

    fix: 에러 수정 진행 중

--- native-app/babel.config.js
+++ native-app/babel.config.js
@@ -2,5 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+        // 이미 있는 플러그인들 유지하고, 아래 라인 추가
+        ['babel-plugin-transform-import-meta', { module: 'es6' }],
+        // expo-router 쓰면 이 플러그인도 보통 있음
+        require.resolve("expo-router/babel"),
+      ],
   };
 };
